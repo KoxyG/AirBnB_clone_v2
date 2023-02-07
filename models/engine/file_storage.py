ad38576edd68ceb0414f0c0a940d57e6befd74c6
Progress Ochuko Eyaadah <koxygraphics@gmail.com>
2023-02-07 14:48:42 -0800

    done

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -51,7 +51,7 @@ class FileStorage:
     def reload(self):
         """deserializes the JSON file to __objects"""
         try:
-            with open(self.__file_path, 'r') as f:
+            with open(self.__file_path, 'r', encoding="UTF-8") as f:
                 jo = json.load(f)
             for key in jo:
                 self.__objects[key] = classes[jo[key]["__class__"]](**jo[key])
